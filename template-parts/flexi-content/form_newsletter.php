d81de03f4e2f6e9d34d10ab690a4f238840664aa
Gary McNally <garymcnally@gmail.com>
2022-11-16 13:27:52 +0000

    replacing CF7 with compaign monitor form

--- template-parts/flexi-content/form_newsletter.php
+++ template-parts/flexi-content/form_newsletter.php
@@ -1,8 +1,62 @@
-<div class="container-fluid contact-form">
+<div class="container-fluid contact-form" id="newsletter">
   <div class="container">
     <div class="row">
 
-      <?php echo do_shortcode('[contact-form-7 id="1727" title="Newsletter"]') ?>
+      <?php //echo do_shortcode('[contact-form-7 id="1727" title="Newsletter"]') ?>
+
+
+
+<div>
+  <h3>Get the latest from <span>Metrowall</span></h3>
+  <form class="js-cm-form row" id="subForm" action="https://www.createsend.com/t/subscribeerror?description=" method="post" data-id="5B5E7037DA78A748374AD499497E309E70A50157BE04207ED8302CC8A480303F01605519DFC98EF503EAD93C97CE675280C6E9D3E34F7E23E76E3482F0BB4329">
+    <!-- <div> -->
+      <div class="col-md-4 form-group">
+        <!-- <label>Name </label> -->
+        <input aria-label="Name" id="fieldName" maxlength="200" name="cm-name" placeholder="Name">
+      </div>
+      <div class="col-md-4 form-group">
+        <!-- <label>Email </label> -->
+        <input autocomplete="Email" aria-label="Email" class="js-cm-email-input qa-input-email" id="fieldEmail" maxlength="200" name="cm-bdyakl-bdyakl" required="" type="email" placeholder="Email">
+      </div>
+      <div class="col-md-4 form-group">
+        <!-- <label>Position </label> -->
+        <input aria-label="Position" id="fieldddfnlu" maxlength="200" name="cm-f-ddfnlu" placeholder="Position">
+      </div>
+      <div class="col-md-6 form-group">
+        <!-- <label>Company Name </label> -->
+        <input aria-label="Company Name" id="fieldddfno" maxlength="200" name="cm-f-ddfno" placeholder="Company Name">
+      </div>
+      <div class="col-md-6 form-group">
+        <!-- <label>Contact Number </label> -->
+        <input aria-label="Contact Number" id="fieldddfnb" maxlength="200" name="cm-f-ddfnb" placeholder="Contact Number">
+      </div>
+      <div class="col-md-12 form-group">
+        <div>
+          <div>
+            <div>
+              <input aria-required="" id="cm-privacy-consent" name="cm-privacy-consent" required="" type="checkbox">
+              <label for="cm-privacy-consent">I agree to be emailed</label>
+            </div>
+            <input id="cm-privacy-consent-hidden" name="cm-privacy-consent-hidden" type="hidden" value="true">
+          </div>
+        </div>
+      </div>
+    <!-- </div> -->
+    <div class="col-md-12 form-group">
+      <button class="newsletter-submit" type="submit">Subscribe</button>
+    </div>
+  </form>
+</div>
+<script type="text/javascript" src="https://js.createsend1.com/javascript/copypastesubscribeformlogic.js"></script>
+
+
+
+
+
+
+
+
+
 
     </div>
   </div>
